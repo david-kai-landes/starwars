089e9b3ac1a3702555595febab9c8c29b93121a1
david-kai-landes <davidkailandes@gmail.com>
2024-10-22 15:15:07 -0400

    added styling to characters.js

--- src/screens/characters.js
+++ src/screens/characters.js
@@ -1,10 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/characters.css";
+import logo from "../img/Star_Wars_Logo.png";
 
 function Characters() {
   return (
-    <div>
-      <h2>Characters of Star Wars</h2>
-      {/* Add content about People here */}
+    <div className="character-screen terminal">
+      <Link to="/">
+        <div className="logoCont">
+          <img src={logo} alt="logo" />
+        </div>
+      </Link>
+      <div className="charactersMainCont">
+        <h2 className="charactersSubtitle">Characters</h2>
+        {/* Future character data will appear here */}
+        <div className="character-highlight">
+          <p>Select a character to view their details...</p>
+        </div>
+      </div>
+      {/* particles */}
+      <div className="particle" />
+      <div className="particle" />
+      <div className="particle" />
+      <div className="particle" />
+      <div className="particle" />
     </div>
   );
 }
--- src/screens/home.js
+++ src/screens/home.js
@@ -10,43 +10,51 @@ import logo from "../img/Star_Wars_Logo.png";
 const Home = () => {
   return (
     <div className="home">
-      <div className="title">
+      <div className="logoCont">
         <img src={logo} alt="logo" />
       </div>
       <div className="card-container">
         <Link to="/characters">
           <div className="card characters">
             <img src={yoda} alt="yoda" />
-            <div>Characters</div>
-            <div className="description">
-              Explore the heroes and villains of the galaxy
+            <div className="textContaioner">
+              <div>Characters</div>
+              <div className="description">
+                Explore the heroes and villains of the galaxy
+              </div>
             </div>
           </div>
         </Link>
         <Link to="/planets">
           <div className="card planets">
             <img src={planets} alt="planets" />
-            <div>Planets</div>
-            <div className="description">
-              Discover distant worlds and their mysteries
+            <div className="textContaioner">
+              <div>Planets</div>
+              <div className="description">
+                Discover distant worlds and their mysteries
+              </div>
             </div>
           </div>
         </Link>
-        <Link to="/starships">
+        <Link className="link" to="/starships">
           <div className="card starships">
             <img src={starship} alt="starship" />
-            <div>Starships</div>
-            <div className="description">
-              See the fastest ships in the galaxy
+            <div className="textContaioner">
+              <div>Starships</div>
+              <div className="description">
+                See the fastest ships in the galaxy
+              </div>
             </div>
           </div>
         </Link>
-        <Link to="/vehicles">
+        <Link className="link_vehicles" to="/vehicles">
           <div className="card vehicles">
             <img src={vehicles} alt="vehicles" />
-            <div>Vehicles</div>
-            <div className="description">
-              Check out iconic vehicles like the AT-AT and speeders
+            <div className="textContaioner">
+              <div>Vehicles</div>
+              <div className="description">
+                Check out iconic vehicles like the AT-AT and speeders
+              </div>
             </div>
           </div>
         </Link>
