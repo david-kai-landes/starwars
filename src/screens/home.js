acc061b1a6c470527c6a9878774b0dd43bd16ce0
david-kai-landes <davidkailandes@gmail.com>
2024-10-21 22:01:03 -0400

    added style to cards

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/home.css";
+import vehicles from "../img/Star_Wars_Vehicles.png";
+import starship from "../img/starship.png";
+import yoda from "../img/Star_Wars_Yoda.png";
+import planets from "../img/planet.png";
 import logo from "../img/Star_Wars_Logo.png";
 
 const Home = () => {
@@ -11,16 +15,40 @@ const Home = () => {
       </div>
       <div className="card-container">
         <Link to="/characters">
-          <div className="card">Characters</div>
+          <div className="card characters">
+            <img src={yoda} alt="yoda" />
+            <div>Characters</div>
+            <div className="description">
+              Explore the heroes and villains of the galaxy
+            </div>
+          </div>
         </Link>
         <Link to="/planets">
-          <div className="card">Planets</div>
+          <div className="card planets">
+            <img src={planets} alt="planets" />
+            <div>Planets</div>
+            <div className="description">
+              Discover distant worlds and their mysteries
+            </div>
+          </div>
         </Link>
         <Link to="/starships">
-          <div className="card">Starships</div>
+          <div className="card starships">
+            <img src={starship} alt="starship" />
+            <div>Starships</div>
+            <div className="description">
+              See the fastest ships in the galaxy
+            </div>
+          </div>
         </Link>
         <Link to="/vehicles">
-          <div className="card">Vehicles</div>
+          <div className="card vehicles">
+            <img src={vehicles} alt="vehicles" />
+            <div>Vehicles</div>
+            <div className="description">
+              Check out iconic vehicles like the AT-AT and speeders
+            </div>
+          </div>
         </Link>
       </div>
     </div>
